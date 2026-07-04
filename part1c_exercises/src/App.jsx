193519ed82085655f90a297f687d84fbf204eb52
macy-august <m-august@onu.edu>
2026-07-03 21:58:01 -0400

    initial commit

---
+++ part1c_exercises/src/App.jsx
@@ -0,0 +1,109 @@
+import Note from './Components/Note'
+import Notification from './Components/Notification'
+import Footer from './Components/Footer'
+import Switch from '@mui/material/Switch'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import noteService from './Services/notes'
+
+const App = () => {
+  const [notes, setNotes] = useState(null)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  useEffect(() => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      })
+  }, [])
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote).then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() > 0.5
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      })
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll ? notes :
+    notes.filter(note => note.important === true)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <FormControlLabel
+          label={showAll ? 'All' : 'Important'}
+          control={
+            <Switch
+              checked={showAll}
+              onChange={() => setShowAll(!showAll)}
+              sx={{
+                '& .MuiSwitch-thumb': {
+                  color: 'blueviolet',
+                },
+                '& .MuiSwitch-track': {
+                  backgroundColor: 'blueviolet !important',
+                },
+              }}
+            />
+          }
+        />
+      </div>
+      <ul>
+        {notesToShow && notesToShow.map((note, index) => 
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)} 
+          />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">Save</button>
+      </form>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
